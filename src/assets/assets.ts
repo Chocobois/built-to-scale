0021395004dad0888fc757d272ce8f506691d668
Luxx Art <luxxtux@gmail.com>
2024-08-20 16:27:46 +0200

    added titlescreen assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -155,10 +155,13 @@ const images: Image[] = [
 	image('ui/plus', 'plus'),
 
 	// Titlescreen
-	image('titlescreen/sky', 'title_sky'),
 	image('titlescreen/background', 'title_background'),
-	image('titlescreen/foreground', 'title_foreground'),
-	image('titlescreen/character', 'title_character'),
+	image('titlescreen/backgroundsitting', 'title_chairs'),
+	image('titlescreen/backgroundbath', 'title_bath'),
+	image('titlescreen/tail', 'title_tail'),
+	image('titlescreen/hand', 'title_hand'),
+	image('titlescreen/washbeartail', 'title_washbear_tail'),
+	image('titlescreen/washbearhand', 'title_washbear_hand'),
 
 	//temp
 	image('temp/invwindow', 'invwindow'),
